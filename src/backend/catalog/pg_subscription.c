3b9ef5e58647693f88d3550f95380b9dc293b91f
Euler Taveira <euler@timbira.com.br>
2018-02-23 18:11:59 -0300

    Subscription assigns custom replication origin identifier

--- src/backend/catalog/pg_subscription.c
+++ src/backend/catalog/pg_subscription.c
@@ -71,6 +71,7 @@ GetSubscription(Oid subid, bool missing_ok)
 	sub->name = pstrdup(NameStr(subform->subname));
 	sub->owner = subform->subowner;
 	sub->enabled = subform->subenabled;
+	sub->roident = subform->subroident;
 
 	/* Get conninfo */
 	datum = SysCacheGetAttr(SUBSCRIPTIONOID,
